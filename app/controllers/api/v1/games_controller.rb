a7cd70df0018cd9cdccbb1159cc6aa8bea5f7f00
Marina P <petrova.dll@gmail.com>
2017-02-27 12:51:36 +0100

    Move leadership query from controller to the model

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -34,20 +34,7 @@ module Api
       end
 
       def leaderboard
-        sql = <<EOF
-SELECT DISTINCT p.name, COUNT(g.*) AS games_won
-FROM players p
-LEFT OUTER JOIN games_players gp
-ON gp.player_id=p.id
-LEFT OUTER JOIN games g
-ON g.id=gp.game_id
-WHERE g.winner_uid IS NOT NULL
-AND g.winner_uid=p.id
-GROUP BY p.name
-ORDER BY games_won DESC;
-EOF
-        result = ActiveRecord::Base.connection.execute(sql).to_a
-        render json: result
+        render json: Game.leaderboard
       end
 
       def start_game
--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,20 @@ class Game < ApplicationRecord
       errors.add(:players, "Two players must be set.")
     end
   end
+
+  def self.leaderboard
+    sql = <<EOF
+SELECT DISTINCT p.name, COUNT(g.*) AS games_won
+FROM players p
+LEFT OUTER JOIN games_players gp
+ON gp.player_id=p.id
+LEFT OUTER JOIN games g
+ON g.id=gp.game_id
+WHERE g.winner_uid IS NOT NULL
+AND g.winner_uid=p.id
+GROUP BY p.name
+ORDER BY games_won DESC;
+EOF
+    return ActiveRecord::Base.connection.execute(sql).to_a
+  end
 end
